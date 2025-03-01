6a91f8aaffeedfb5b55fdfad3394f0de55458d39
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-01 17:17:10 -0500

    build: extended colors added to tailwind - Colors were added for: gray, background and black

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          'dark': '#DBDBDB',
+          'medium': 'f0f0f0',
+          'DEFAULT': '#f0f0f0',
+          'light': '#F0F0F0'
+        },
+        background: {
+          'DEFAULT': '#FAFAFA',
+          'light': '#FAFAFA'
+        },
+        black: {
+          'dark': '#000000',
+          'DEFAULT': '#000000',
+          'light': '#333333'
+        }
+      }
+    },
   },
   plugins: [],
 }
