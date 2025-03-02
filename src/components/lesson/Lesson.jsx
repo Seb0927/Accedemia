f626916f3d769870da52133c48f5b967a1645c98
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-01 20:56:02 -0500

    feat (lesson): create mock component

---
+++ src/components/lesson/Lesson.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Lesson = () => {
+  return (
+    <div className='h-full w-[25%] bg-gray-dark border border-black'>
+
+    </div>
+  )
+}
+
+export default Lesson
