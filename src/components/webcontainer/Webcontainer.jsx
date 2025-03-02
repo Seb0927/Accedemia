1fa0570223b3eab6c6cd9616520dd733177c1a23
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-01 20:55:45 -0500

    feat (webcontainer): create component

---
+++ src/components/webcontainer/Webcontainer.jsx
@@ -0,0 +1,13 @@
+import Code from './Code'
+import Page from './Page'
+
+const Webcontainer = () => {
+  return (
+    <>
+      <Code />
+      <Page />
+    </>
+  )
+}
+
+export default Webcontainer
