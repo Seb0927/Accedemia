d9d32657915c283efa097cf95cd19be218d819d2
Seb0927 <sebastianidrobo@hotmail.com>
2025-03-03 20:48:55 -0500

    feat(directory): add directory component
    - TO-DO: Make it functional

--- src/components/webcontainer/Code.jsx
+++ src/components/webcontainer/Code.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Directory from './Directory'
 
 const Code = () => {
   return (
-    <div className='h-full w-[30%] bg-gray-dark border border-black p-2'>
-
+    <div className='h-full w-[30%] bg-gray-dark border border-black flex flex-col-reverse p-2'>
+      <Directory />
     </div>
   )
 }
---
+++ src/components/webcontainer/Directory.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Directory = () => {
+  return (
+    <div className='h-1/5 w-full bg-background border border-black'>
+    </div>
+  )
+}
+
+export default Directory
