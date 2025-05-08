16eefe2a512961fdd2d493d469d207bb7b171072
Seb0927 <sebastianidrobo@hotmail.com>
2025-05-07 22:24:32 -0500

    fix(sidebar): update width of sidebar component for better responsiveness

--- src/components/background/Sidebar.jsx
+++ src/components/background/Sidebar.jsx
@@ -6,7 +6,7 @@ const sidebar = (props) => {
   const lessons = Array.from({ length: 31 }, (_, i) => `LECCIÓN ${i + 1}`);
 
   return (
-    <div className='fixed top-0 left-0 h-full w-1/5 bg-gray-dark overflow-auto'>
+    <div className='fixed top-0 left-0 h-full w-1/2 md:w-1/3 lg:w-1/4 bg-gray-dark overflow-auto'>
       <ul className=''>
         <li className='flex items-center h-16 border border-black hover:bg-gray-600 hover:text-slate-100'>
           <button onClick={() => setSidebar(false)} className='h-full w-full text-left text-xl font-semibold font-mono px-4'>
