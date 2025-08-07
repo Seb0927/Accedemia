e3d4bd37bc082ab0918955119b33d827c69f8510
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2025-08-07 01:52:35 +0000

    feat(Navbar): add Navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Menu } from "lucide-react";
+import Logo from "@/components/Logo";
+
+function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost">
+            <Menu />
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm md:menu-md dropdown-content bg-base-100 rounded-box z-1 mt-4 w-52 p-2 shadow">
+            <li><Link href='/about'>Nosotros</Link></li>
+            <li><Link href='/'>Lecciones</Link></li>
+            <li><Link href='/credits'>Créditos</Link></li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <Logo className='h-6 w-64 md:h-7' />
+      </div>
+      <div className="navbar-end">
+
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
