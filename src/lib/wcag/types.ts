cfb40e8a2eb23e4a3779dcf9d1cde2e158b8dec3
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2025-08-07 16:51:32 +0000

    feat(WCAG): add wcag lib for extracting information from curriculum submodule

---
+++ src/lib/wcag/types.ts
@@ -0,0 +1,21 @@
+export interface SuccessCriterion {
+  name: string;
+  level: string;
+  description: string;
+}
+
+export interface Guideline {
+  name: string;
+  description: string;
+  criteria: Record<string, SuccessCriterion>;
+}
+
+export interface Principle {
+  name: string;
+  description: string;
+  guidelines: Record<string, Guideline>;
+}
+
+export interface WcagRuleMap {
+  [principleNum: string]: Principle;
+}
