6d206658356e1c70d3e02be5e1497033ef1bac94
Seb0927 <83418390+Seb0927@users.noreply.github.com>
2025-08-07 16:54:32 +0000

    refactor(Navbar): add minimum height and z-index to navbar component - The z-index is for not disturbing the sidebar drawer layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from "@/components/Logo";
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar min-h-14 bg-base-100 shadow-sm z-1">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost">
