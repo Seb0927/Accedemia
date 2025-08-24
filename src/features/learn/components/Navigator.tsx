da9faaebb2ef65044823be8c79f6934378c8b4f0
Seb0927 <sebastianidrobo@hotmail.com>
2025-08-23 23:48:29 -0500

    fix(Navigator): add spacing betwen items

--- src/features/learn/components/Navigator.tsx
+++ src/features/learn/components/Navigator.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from "@/features/learn/components/Breadcrumbs";
 
 function Navigator() {
   return (
-    <div className="navbar bg-base-100 min-h-11 px-3 py-1 shadow-sm">
+    <div className="navbar bg-base-100 min-h-11 px-3 py-1 shadow-sm flex gap-2">
       <input id="lessons-drawer" className="drawer-toggle" type="checkbox" />
       <label className="btn btn-sm drawer-button p-2" htmlFor="lessons-drawer">
         <Menu className="size-4" />
