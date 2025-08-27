16401181254694cb3651922d9d912e44abdf0edd
Seb0927 <sebastianidrobo@hotmail.com>
2025-08-26 23:58:50 -0500

    fix(Card): remove unnecessary padding from Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ function Card({ className, children }: CardProps) {
   return (
     <div className={cn("h-full card card-border shadow-md relative", className)}>
       <div className="h-full card-body p-0 relative">
-        <div className="overflow-auto p-6 h-full">
+        <div className="overflow-auto h-full">
           {children}
         </div>
       </div>
