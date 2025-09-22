b16abd41dc23d6eb68fa99b4b7b91e9537c5a10a
Seb0927 <sebastianidrobo@hotmail.com>
2025-09-22 10:30:48 -0500

    build(next.config.ts): add TypeScript support and configure markdown file handling

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   async headers() {
     return [
       {
@@ -18,6 +19,13 @@ const nextConfig = {
       },
     ];
   },
+  webpack: (config) => {
+    config.module?.rules?.push({
+      test: /\.md$/,
+      type: 'asset/source',
+    });
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
