41dcd4d5ad67f0c0e72f68b491ad1748ff07d3dd
Seb0927 <sebastianidrobo@hotmail.com>
2025-09-23 22:02:24 -0500

    ci: apply modifications in order to comply with azure static web apps deployment - documentation: https://learn.microsoft.com/en-us/azure/static-web-apps/deploy-nextjs-hybrid

--- src/middleware.ts
+++ src/middleware.ts
@@ -20,5 +20,11 @@ export function middleware(request: NextRequest) {
 
 // Specify which paths this middleware should run on
 export const config = {
-  matcher: ["/"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - .swa (Azure Static Web Apps)
+     */
+    "/((?!.swa).*)",
+  ],
 };
